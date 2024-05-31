f975e6774c4376dac9804c3d5f2c96ecda51e981
siesto1elemento <rohitojha9720@gmail.com>
2024-06-01 01:07:04 +0530

    create Registration form

--- freelance/settings.py
+++ freelance/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_countries',
     'freelanceapp',
     'users',
 ]
@@ -124,3 +125,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "users.CustomUser"
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from .models import CustomUser
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        country = request.POST.get('country')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        
+        role = request.POST.get('role')
+        is_client = (role == 'client')
+        is_freelancer = (role == 'freelancer')
+        
+
+        if password1 != password2:
+            return render(request,"users/registration.html")
+
+            
+            
+
+        new_profile = CustomUser(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            country=country,
+            is_client=is_client,
+            is_freelancer=is_freelancer,
+            
+        )
+        new_profile.set_password(password1)
+        new_profile.save()
+
+
+
+
+        return render(request,"users/registration.html")
+
+    else:
+        return render(request,"users/registration.html")
+    
