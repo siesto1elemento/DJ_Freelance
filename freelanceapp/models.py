13868a2dfd19e828f0c75cb8caa7dbda33c06922
siesto1elemento <rohitojha9720@gmail.com>
2024-06-08 21:32:09 +0530

    built profile model for client snd freelancer

--- freelanceapp/models.py
+++ freelanceapp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from users.models import CustomUser
+from phonenumber_field.modelfields import PhoneNumberField
+import pytz
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+
+class Client(models.Model):
+    client = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=255)
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+    address = models.TextField()
+    TIMEZONES = tuple(zip(pytz.all_timezones, pytz.all_timezones))
+    timezone = models.CharField(max_length=32, choices=TIMEZONES, default='UTC')
+    postal_code = models.CharField(
+        max_length=10,
+        validators=[
+            RegexValidator(
+                regex=r'^[0-9A-Za-z\s-]+$',
+                message='Enter a valid postal code.'
+            )
+        ]
+    )
+
+class Freelancer(models.Model):
+    freelancer = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+    address = models.TextField()
+    TIMEZONES = tuple(zip(pytz.all_timezones, pytz.all_timezones))
+    timezone = models.CharField(max_length=32, choices=TIMEZONES, default='UTC')
+    postal_code = models.CharField(
+        max_length=10,
+        validators=[
+            RegexValidator(
+                regex=r'^[0-9A-Za-z\s-]+$',
+                message='Enter a valid postal code.'
+            )
+        ]
+    )
+    
+
+
--- freelanceapp/views.py
+++ freelanceapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from users.models import CustomUser
 
 def home(request):
     return render(request, "freelanceapp/home.html")
@@ -7,4 +8,8 @@ def home(request):
 
 @login_required(login_url='/users/login')
 def freelancer_view(request):
+    users = CustomUser.objects.all()
+    
+
     return render(request,"freelanceapp/freelancer.html")
+
